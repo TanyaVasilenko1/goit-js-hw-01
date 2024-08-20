f808a77df5f6e8eb6087f9dc58a7488f4c1ee55e
Tanya Vasilenko <tanyusha.vasilenko.11.09@gmail.com>
2024-08-20 20:36:03 +0300

    fix errors

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,10 @@
 function getElementWidth(content,padding,border){
-   return Number.parseFloat(content) + 2 * (Number.parseFloat(padding) + Number.parseFloat(border));
+    const contentnum = Number.parseFloat(content);
+    const paddingnum = Number.parseFloat(padding);
+    const bordernum = Number.parseFloat(border);
+    const result = contentnum + 2 *(paddingnum + bordernum);
+    return result;
 }
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
